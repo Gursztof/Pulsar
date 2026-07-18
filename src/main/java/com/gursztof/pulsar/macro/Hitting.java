8204f6fdf51f2bab6e9926f955202b2134da9361
Gursztof <krzyskurek08@gmail.com>
2026-07-18 20:09:21 +0200

    Added hitting feture

---
+++ src/main/java/com/gursztof/pulsar/macro/Hitting.java
@@ -0,0 +1,16 @@
+package com.gursztof.pulsar.macro;
+
+import net.minecraft.client.MinecraftClient;
+
+public class Hitting {
+    // There is small chance that mc won't be init yet so it might cause the crash
+    private static final MinecraftClient minecraftInstance = MinecraftClient.getInstance();
+
+    public static void start() {
+        minecraftInstance.options.attackKey.setPressed(true);
+    }
+
+    public static void start() {
+        minecraftInstance.options.attackKey.setPressed(true);
+    }
+}
